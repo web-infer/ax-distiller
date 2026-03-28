85fe9c8be1408784e55f3e18438df464841afb17
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-28 16:17:15 -0700

    add flakes

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default =
+        let
+          libs = with pkgs; [
+            fuse
+            stdenv.cc.cc
+            glib
+            nss
+            nspr
+            dbus
+            at-spi2-atk
+            cups
+            libdrm
+            expat
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            libgbm
+            xorg.libxcb
+            libxkbcommon
+            pango
+            cairo
+            alsa-lib
+            fuse
+            libz
+            libudev-zero
+          ];
+        in
+        pkgs.mkShell {
+          name = "devenv";
+          buildInputs = libs;
+          nativeBuildInputs = (
+            with pkgs;
+            [
+              pkg-config
+            ]
+          );
+
+          NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH";
+
+          shellHook = ''
+            echo "Devshell activated."
+          '';
+        };
+    };
+}
