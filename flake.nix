aeb0ea5e246148208074ab754aeb717d775b9dbe
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-28 22:54:35 -0700

    use ungoogled-chromium in flake

--- flake.nix
+++ flake.nix
@@ -12,31 +12,32 @@
       devShells.${system}.default =
         let
           libs = with pkgs; [
-            fuse
-            stdenv.cc.cc
-            glib
-            nss
-            nspr
-            dbus
-            at-spi2-atk
-            cups
-            libdrm
-            expat
-            xorg.libX11
-            xorg.libXcomposite
-            xorg.libXdamage
-            xorg.libXext
-            xorg.libXfixes
-            xorg.libXrandr
-            libgbm
-            xorg.libxcb
-            libxkbcommon
-            pango
-            cairo
-            alsa-lib
-            fuse
-            libz
-            libudev-zero
+            ungoogled-chromium
+            # fuse
+            # stdenv.cc.cc
+            # glib
+            # nss
+            # nspr
+            # dbus
+            # at-spi2-atk
+            # cups
+            # libdrm
+            # expat
+            # xorg.libX11
+            # xorg.libXcomposite
+            # xorg.libXdamage
+            # xorg.libXext
+            # xorg.libXfixes
+            # xorg.libXrandr
+            # libgbm
+            # xorg.libxcb
+            # libxkbcommon
+            # pango
+            # cairo
+            # alsa-lib
+            # fuse
+            # libz
+            # libudev-zero
           ];
         in
         pkgs.mkShell {
